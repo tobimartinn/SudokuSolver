bd6365c81f700b44ba5f9cc57ee67bff2d8f5609
Tobias Martin <tcaputo@frba.utn.edu.ar>
2022-12-07 19:43:53 -0300

    se agrego implementacion con numpy

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from CommonFunctions import check_number, calculate_next_el
+import numpy as np
 
 
 def print_sudoku(sudoku):
@@ -13,30 +14,31 @@ current_element = [0, 0]
 
 
 
-def sudoku_solver(list, current_element):
+
+def sudoku_solver(sudoku, current_element):
     current_row = current_element[0]
     current_column = current_element[1]
 
     if current_element == [0,0]:
-        if list[current_row][current_column] != 0:
-            current_element = calculate_next_el(list, current_element)
+        if sudoku[current_row][current_column] != 0:
+            current_element = calculate_next_el(sudoku, current_element)
             current_row = current_element[0]
             current_column = current_element[1]
 
     for i in range(9):
-        list[current_row][current_column] += 1
-        if check_number(list, current_element):
-            next_element = calculate_next_el(list, current_element)
+        sudoku[current_row][current_column] += 1
+        if check_number(sudoku, current_element):
+            next_element = calculate_next_el(sudoku, current_element)
             if next_element == [9, 0]:
-                print_sudoku(list)
+                print_sudoku(sudoku)
                 return True
             else:
-                if sudoku_solver(list, next_element):
+                if sudoku_solver(sudoku, next_element):
                     return True
         else:
             pass
     else:
-        list[current_row][current_column] = 0
+        sudoku[current_row][current_column] = 0
         return False
 
 
@@ -60,7 +62,20 @@ sudok2 = [[5, 0, 2, 0, 8, 0, 0, 0, 0],
           [0, 2, 0, 9, 0, 5, 0, 3, 0],
           [0, 0, 0, 0, 6, 0, 4, 0, 2]]
 
+sudoku2 = np.array([[5, 0, 2, 0, 8, 0, 0, 0, 0],
+                    [0, 7, 0, 5, 0, 1, 0, 9, 0],
+                    [0, 0, 0, 2, 0, 0, 0, 0, 0],
+                    [4, 0, 0, 6, 0, 0, 0, 0, 7],
+                    [0, 1, 8, 0, 7, 0, 9, 6, 0],
+                    [7, 0, 0, 0, 0, 2, 0, 0, 4],
+                    [0, 0, 0, 0, 0, 3, 0, 0, 0],
+                    [0, 2, 0, 9, 0, 5, 0, 3, 0],
+                    [0, 0, 0, 0, 6, 0, 4, 0, 2]], dtype='uint8')
+
+
+
 sudoku_solver(sudok2, current_element)
+
 
 
 
